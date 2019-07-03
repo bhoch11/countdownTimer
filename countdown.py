ba313349838c510cb02a159df0141e9a99e627e0
Blake Hochstettler <bhochstettler11@gmail.com>
2019-07-03 08:03:54 -0500

    Update countdown.py
    Updated countdown timer to properly overwrite previous field as well as add in exception handling and recursion until proper values have been entered by the user.

--- countdown.py
+++ countdown.py
@@ -1,14 +1,43 @@
+
+#import modules
 import time
 import sys
-import tkinter
 
-#begins countdown, loops through minutes and seconds, prints each value
-def countdown():
+# Function for obtaining a minute value and validating it.
+def chooseMinute():
         global minute
+        try:
+            minute = int(input())
+        except ValueError:
+            print('That is not a valid integer, try again')
+            chooseMinute()
+        if minute > -1:
+            return minute
+        else:
+            print('Please enter a non-negative integer')
+            chooseMinute()
+
+# Function for obtaining a seconds value and validating it.
+def chooseSeconds():
         global seconds
+        try:
+            seconds = int(input())
+        except ValueError:
+            print('That is not a valid integer, try again')
+            chooseSeconds()
+        if seconds > -1 and seconds < 60:
+            return seconds
+        else:
+            print('Please enter a value between 0 and 59')
+            chooseSeconds()
+
+
+
+#begins countdown, loops through minutes and seconds, overwriting original value.
+def countdown(minute, seconds):
         print('beginning countdown')
         if minute > 0 and seconds == 0:  ## working
-                sys.stdout.write(str(minute)+':0'+str(seconds))
+                sys.stdout.write("\r" + str(minute)+':0'+str(seconds))
                 sys.stdout.flush()
                 time.sleep(1)
                 minute = minute - 1
@@ -16,49 +45,47 @@ def countdown():
                 for num in range(0, minute + 1):
                         for num in range(0, 60):
                                 if seconds >= 10:
-                                        sys.stdout.write(str(minute) +':'+str(seconds))
+                                        sys.stdout.write("\r" + str(minute) +':'+str(seconds))
                                         sys.stdout.flush()
                                 else:
-                                        sys.stdout.write(str(minute)+':0'+str(seconds))
+                                        sys.stdout.write("\r" + str(minute)+':0'+str(seconds))
                                         sys.stdout.flush()
                                 seconds = seconds - 1
                                 time.sleep(1)
                         minute = minute - 1
                         seconds = 59
-                        
-        elif minute > 0 and seconds > 0:  ## working
+
+        elif minute > 0 and seconds > 0:
                 for num in range(0, minute + 1):
                         for num in range(0 , seconds + 1):
                                 if seconds >= 10:
-                                        sys.stdout.write(str(minute)+':'+str(seconds))
+                                        sys.stdout.write("\r" + str(minute)+':'+str(seconds))
                                         sys.stdout.flush()
                                 else:
-                                        sys.stdout.write(str(minute)+':0'+str(seconds))
+                                        sys.stdout.write("\r" + str(minute)+':0'+str(seconds))
                                         sys.stdout.flush()
                                 seconds -= 1
                                 time.sleep(1)
                         minute = minute - 1
                         seconds = 59
-                        
+
         else:                           ## working
                 for num in range(0, seconds + 1):
                         if seconds >= 10:
-                                sys.stdout.write(str(minute)+':'+str(seconds))
+                                sys.stdout.write("\r" + str(minute)+':'+str(seconds))
                                 sys.stdout.flush()
                         else:
-                                sys.stdout.write(str(minute)+':0'+str(seconds))
+                                sys.stdout.write("\r" + str(minute)+':0'+str(seconds))
                                 sys.stdout.flush()
                         seconds = seconds - 1
                         time.sleep(1)
 
-                             
-#takes user input to store in variable                
-print('Please enter a minute value')
-minute = int(input())
-print('Please enter a second value')
-seconds = int(input())
 
-#program function calls		
-countdown()
+#running the program.
+print('Please enter a value for Minutes')
+chooseMinute()
 
+print('Please enter a value for Seconds')
+chooseSeconds()
 
+countdown(minute, seconds)
